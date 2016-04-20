22bd5182db4eff4c6667d33d2479454edef594a0
Mitchell Fream <M1Fream@gmail.com>
2016-04-20 11:03:34 -0400

    gui updates

--- src/GUI.java
+++ src/GUI.java
@@ -1,21 +1,43 @@
 import java.awt.*;
+
 import javax.swing.*;
 
 
-public class GUI {
+public class GUI extends JFrame{
+	private JLabel fail;
 	
-	public static void main(String[] args){
-		JFrame frame = new JFrame("Prom Sign Out"); //Instantiate frame for GUI
-		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);//Default is to do nothing on close (WindowListener handles closing and asks for password)
+	public GUI(String string) {
+		super(string);
+		main();
+	}
+
+	public void main(){
+		//JFrame frame = new JFrame("Prom Sign Out"); //Instantiate frame for GUI
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);//Default is to do nothing on close (WindowListener handles closing and asks for password)
 	
-		frame.setLayout(new BorderLayout()); //Set frame's layout
+		setLayout(new BorderLayout()); //Set frame's layout
 		
-		PromPanel promPanel = new PromPanel(); 
-		frame.add(promPanel,BorderLayout.CENTER); //Add PromPanel to center of frame
+		//PromPanel promPanel = new PromPanel(); 
+		//add(promPanel,BorderLayout.CENTER); //Add PromPanel to center of frame
 		
-		frame.setSize(1650, 1080); //Default frame size
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //Sets frame to max size
-		frame.setVisible(true); //Makes frame actually visible
+		setSize(1280, 1024); //Default frame size
+		setExtendedState(JFrame.MAXIMIZED_BOTH); //Sets frame to max size
+		setVisible(true); //Makes frame actually visible
+		
+		fail = new JLabel("Invalid");
+		add(fail);
+		fail.setVisible(false);
+	}
+
+	public void badID() {
+		fail.setVisible(true);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+		fail.setVisible(false);
 	}
 	
 	
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter the password used to stop the program: ");
 		Globals.pass=in.next();
-		GUI gui = new GUI();
+		GUI gui = new GUI("Prom sign out");
 		while (!Globals.done) {
 			try{
 				System.out.print("Enter ID: ");
@@ -16,10 +16,11 @@ public class Main {
 				Student temp = new Student(ID); // student will do it's own database lookups
 				System.out.println(temp);
 			} catch (IDoutOfRangeException e) {
-				gui.IDoutOfRange();
+				gui.badID();
 				System.out.println("Out of range");
 			} catch (InputMismatchException e) {
 				System.out.println("ID too big or contains letters");
+				gui.badID();
 				in.next(); //this is magic
 			}
 		}
