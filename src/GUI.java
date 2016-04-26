ab7160e26133939034a9df592cbe28aec11441df
Rosey1021 <samkrosenberg@gmail.com>
2016-04-25 23:49:27 -0400

    Update GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,50 +1,42 @@
 import java.awt.*;
-
 import javax.swing.*;
 
 
 public class GUI extends JFrame{
-	private JLabel fail;
-	private Component a;
+	private static CustomDialog cd;
+	private static PasswordDialog pd;
 	
-	public GUI(String string) {
+	public GUI(String string){
 		super(string);
-		main();
 	}
-
+	
 	public void main(){
-		//JFrame frame = new JFrame("Prom Sign Out"); //Instantiate frame for GUI
-		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);//Default is to do nothing on close (WindowListener handles closing and asks for password)
-	
-		setLayout(new BorderLayout()); //Set frame's layout
+			//JFrame frame = new JFrame("Prom Sign Out"); //Create frame for GUI
+			setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE); //Default is do nothing on close
+			//Set frame's layout
+			setLayout(new BorderLayout()); 
 		
-		//PromPanel promPanel = new PromPanel(); 
-		//add(promPanel,BorderLayout.CENTER); //Add PromPanel to center of frame
+			//Add PromPanel to center of frame
+			PromPanel promPanel = new PromPanel(); 
+			add(promPanel,BorderLayout.CENTER); 
 		
-		setSize(1280, 1024); //Default frame size
-		setExtendedState(JFrame.MAXIMIZED_BOTH); //Sets frame to max size
-		setVisible(true); //Makes frame actually visible
-		
-		//a=new Component();
-		//add(a);
-		//a.start();
-		//a.setVisible(true);
-		
-		fail = new JLabel("Invalid");
-		add(fail);
-		fail.setVisible(false);
+			setSize(1280, 1024); //Default frame size
+			setExtendedState(JFrame.MAXIMIZED_BOTH); //Set frame to max size 
+			setVisible(true); //Make frame actually visible
+			
+			pd = new PasswordDialog(this);
+			pd.setVisible(true);
+			
+			cd = new CustomDialog(this, promPanel.getPass());
 	}
-
-	public void badID() {
-		fail.setVisible(true);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		}
-		fail.setVisible(false);
+			
+	//Toggle cd's visibility
+	public static void setDialogVisible(boolean b){
+		cd.setVisible(b);
 	}
 	
-	
+	public void close(){
+		setVisible(false);
+		dispose();
+	}
 }
