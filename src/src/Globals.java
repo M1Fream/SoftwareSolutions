585afb062185bdec54eeed8bb32fbf5f54c81232
Rosey1021 <samkrosenberg@gmail.com>
2016-04-28 21:41:25 -0400

    Hopefully this fixed whatever just happened

--- src/src/Globals.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-
-public class Globals {
-	public static boolean done=false;
-	public static String pass;
-	public static ArrayList<Student> studentList = new ArrayList<Student>();
-}
--- src/src/IDoutOfRangeException.java
+++
@@ -1,9 +0,0 @@
-
-public class IDoutOfRangeException extends Exception {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-}
