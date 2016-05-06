79b27c59facc870416dcf51ffc6d0cc7d76c9df8
Rosey1021 <samkrosenberg@gmail.com>
2016-05-06 12:39:45 -0400

    Actually done now

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		IO.init(); // must be here, don't touch
+		//IO.init(); // must be here, don't touch
 		//System.out.println(IO.get(372290,"GC"));
 		GUI gui = new GUI("Prom sign out");
 		gui.main();
